f89d8c347302b639b195539311b3823f3ab72ac0
celineMend <celine.mendy@unchk.edu.sn>
2024-10-05 18:16:08 +0000

    structuration du projet

---
+++ src/app/Models/conducteur/conducteur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conducteur',
+  standalone: true,
+  imports: [],
+  templateUrl: './conducteur.component.html',
+  styleUrl: './conducteur.component.css'
+})
+export class ConducteurComponent {
+
+}
---
+++ src/app/services/trajet/trajet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trajet',
+  standalone: true,
+  imports: [],
+  templateUrl: './trajet.component.html',
+  styleUrl: './trajet.component.css'
+})
+export class TrajetComponent {
+
+}
